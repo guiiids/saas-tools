1223a90bb71a9256610b513244679536dbe6d386
guiiids <gui.vieiraa@gmail.com>
2025-03-09 14:51:01 -0400

    Add .gitignore, sendGrid.py, and text-editor.html

---
+++ sendGrid.py
@@ -0,0 +1,18 @@
+# https://github.com/sendgrid/sendgrid-python
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+message = Mail(
+    from_email='gui.vieiraa@gmail.com',
+    to_emails='gui.vieiraa2@gmail.com',
+    subject='Sending with Twilio SendGrid is Fun',
+    html_content='<strong>and easy to do anywhere, even with Python</strong>')
+try:
+    sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+    response = sg.send(message)
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+except Exception as e:
+    print(str(e))  # Use str(e) to get the error message
